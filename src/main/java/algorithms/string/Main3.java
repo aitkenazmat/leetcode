d740d9e14fe59eeaa116048f0e7f907034dd1438
azaitken <aitken.azamat@gmail.com>
2021-08-27 12:01:55 +0600

    шаблон для m-3

---
+++ src/main/java/algorithms/string/Main3.java
@@ -0,0 +1,11 @@
+package algorithms.string;
+
+public class Main3 {
+    public static void main(String[] args) {
+        lengthOfLongestSubstring("azadd");
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+}
